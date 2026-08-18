cc379876a0cf31802982916286a17fd451d1318d
irasov <srnan@yandex.ru>
2026-08-18 12:24:44 +0500

    feat: add main styles and Header

---
+++ excel-lego/src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Header.module.scss";
+
+const Header: React.FC = () => {
+  return (
+    <div className={styles.body}>HEADER</div>
+  )
+}
+
+export default Header;
