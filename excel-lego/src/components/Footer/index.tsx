485a22a15a9ae47a9a7288e4721e215f16df7572
irasov <srnan@yandex.ru>
2026-08-27 16:44:59 +0500

    feat: add footer

--- excel-lego/src/components/Footer/index.tsx
+++ excel-lego/src/components/Footer/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from './Footer.module.scss';
+import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="container">
         <div className={styles.body}>
-          <h2 className={styles.title}>Footer</h2>
+          <Link to="/" className={styles.link}>
+            <h2 className={styles.title}>Brick & Glow</h2>
+          </Link>
+          <span className={styles.text}>©2026 Brick & Glow</span>
         </div>
       </div>
     </footer>
